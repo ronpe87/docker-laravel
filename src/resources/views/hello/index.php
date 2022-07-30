1bc08e738c66409703a669682dda194fed16d7f4
kk <150450050@ccalumni.meijo-u.ac.jp>
2022-07-30 17:55:02 +0900

    docker作成

---
+++ src/resources/views/hello/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <title>Hello/index</title>
+  <style>
+    body {font-size:16pt; color:#999; }
+    h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
+  </style>
+</head>
+<body>
+  <h1>Index</h1>
+  <p><?php echo $msg; ?></p>
+  <p><?php echo $id; ?></p>
+</body>
+</html>
---
+++ src/routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('hello/{msg?}', function ($msg='no message.') {
+// $html = <<<EOF
+// <html>
+// <head>
+// <title>Hello</title>
+// <style>
+// body {font-size:16pt; color:#999; }
+// h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
+// </style>
+// </head>
+// <body>
+//     <h1>Hello</h1>
+//     <p>{$msg}</p>
+// </body>
+// </html>
+// EOF;
+//     return $html;
+// });
+
+Route::get('hello/{id}', 'App\Http\Controllers\HelloController@index');
+Route::get('hello/other', 'App\Http\Controllers\HelloController@other');
