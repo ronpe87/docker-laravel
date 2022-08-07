76ab3f975ebf37ebd59976f4668f36a58b25fe5d
kk <150450050@ccalumni.meijo-u.ac.jp>
2022-08-07 12:27:21 +0900

    サービスプロバイダの利用 $view->with('ビューコンポーザを割り当てるビュー', '実行する処理かクラス');

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -22,12 +22,7 @@ class HelloController extends Controller
 {
     public function index()
     {
-        $data = [
-            ['name'=>'山田太郎', 'mail'=>'taro@yamada'],
-            ['name'=>'山田花子', 'mail'=>'hanako@yamada'],
-            ['name'=>'山田幸子', 'mail'=>'sachiko@yamada']
-        ];
-        return view('hello.index', ['data'=>$data]);
+        return view('hello.index', ['message'=>'Hello!']);
     }
 
     public function post(Request $request)
